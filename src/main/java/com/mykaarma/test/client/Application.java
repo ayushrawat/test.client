ef60ffa5dd1870ea0924f2da38c7ef4bf61ec154
ayushrawat <ayush.rawat@kaarya.com>
2018-03-24 23:19:09 +0530

    TEST.CLIENT.6

--- src/main/java/com/mykaarma/test/client/Application.java
+++ src/main/java/com/mykaarma/test/client/Application.java
@@ -43,6 +43,8 @@ public class Application {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(queueName);
+        container.setConcurrentConsumers(50);
+        container.setPrefetchCount(30);
         container.setMessageListener(listenerAdapter);
         return container;
     }
